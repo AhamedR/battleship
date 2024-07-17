aeece6381b05ef0c2ec7f9b27ac29f693b5095ac
Ahamed <ahamedrasheedhot@gmail.com>
2024-07-17 17:48:11 +0530

    feat: utilize the hook to display destroyed areas

--- src/components/molecules/ShipList.tsx
+++ src/components/molecules/ShipList.tsx
@@ -2,55 +2,18 @@ import { useMemo } from 'react';
 import Grid from '@mui/material/Grid';
 
 import Ship from '../atoms/Ship';
+import useGameLogic from '../../hooks/useGameLogic';
 
-// Assets
-import CarrierImg from '../../assets/carrier.png';
-import BattleshipImg from '../../assets/battleship.png';
-import CruiserImg from '../../assets/cruiser.png';
-import SubmarineImg from '../../assets/submarine.png';
-import DestroyerImg from '../../assets/destroyer.png';
-
-const shipsData = [
-  {
-    name: 'Carrier',
-    size: 5,
-    img: CarrierImg,
-    hits: [false, false, false, false, false],
-  },
-  {
-    name: 'Battleship',
-    size: 4,
-    img: BattleshipImg,
-    hits: [false, false, false, false],
-  },
-  {
-    name: 'Cruiser',
-    size: 3,
-    img: CruiserImg,
-    hits: [false, false, false],
-  },
-  {
-    name: 'Submarine',
-    size: 3,
-    img: SubmarineImg,
-    hits: [false, false, false],
-  },
-  {
-    name: 'Destroyer',
-    size: 2,
-    img: DestroyerImg,
-    hits: [true, true],
-  },
-];
 
 const ShipList: React.FC = () => {
-  const ships = useMemo(() => shipsData, []);
+  const {ships} = useGameLogic()
+  const shipData = useMemo(() => ships, [ships]);
 
   return (
     <>
-      {ships.map((ship, index) => (
-        <Grid key={index} item xs={12} sm={6} md={12} maxWidth="50%">
-          <Ship name={ship.name} img={ship.img} hits={ship.hits} />
+      {Object.keys(shipData).map((ship) => (
+        <Grid key={ship} item xs={12} sm={6} md={12} maxWidth="50%">
+          <Ship name={ship} img={ships[ship].img} hits={ships[ship].hits} />
         </Grid>
       ))}
     </>
