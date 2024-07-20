7d718987b194d432b6d6f77fac220e1ec378b5e4
Ahamed <ahamedrasheedhot@gmail.com>
2024-07-20 15:36:44 +0530

    fix: remove disable option from button

--- src/components/atoms/Cell.tsx
+++ src/components/atoms/Cell.tsx
@@ -28,7 +28,6 @@ const Cell: React.FC<CellProps> = ({ value, onClick }) => {
       <Button
         onClick={onClick}
         aria-label={`cell ${value}`}
-        disabled={!!value}
         sx={{ padding: "0", width: "100%", minWidth: "auto", height: "auto" }}
       >
         <img
