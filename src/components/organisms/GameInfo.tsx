adc23010ac8bde2f1d46d10cb96a43dadac1f950
Ahamed <ahamedrasheedhot@gmail.com>
2024-07-20 12:55:09 +0530

    feat: optimize useGameLogic hook with GameInfo

--- src/components/organisms/GameInfo.tsx
+++ src/components/organisms/GameInfo.tsx
@@ -2,13 +2,16 @@ import Grid from "@mui/material/Grid";
 
 import ScoreBoard from "../molecules/ScoreBoard";
 import ShipList from "../molecules/ShipList";
+import useGameLogic from "../../hooks/useGameLogic";
 
 const GameInfo: React.FC = () => {
+  const { user, opponent } = useGameLogic();
+
   return (
     <Grid container sx={{ flexDirection: { md: "row", sm: "row" } }}>
       <Grid container item md={12} xs={12} sm={4} mb={2}>
-        <ScoreBoard name="Player 1" score={1} />
-        <ScoreBoard name="Player 2" score={0} color="#00ACC1" />
+        <ScoreBoard name={user.name} score={user.score} color="#FFB300" />
+        <ScoreBoard name={opponent.name} score={opponent.score} color="#00ACC1" />
       </Grid>
       <Grid container item md={12} xs={12} sm={8} pl={{ sm: 2 }}>
         <ShipList />
