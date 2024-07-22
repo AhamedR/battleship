86b0059a947d602f93b0ca684c00505fc8bbf35d
Ahamed <ahamedrasheedhot@gmail.com>
2024-07-22 08:45:52 +0530

    feat: integrate skeleton in board comp

--- src/components/organisms/Board.tsx
+++ src/components/organisms/Board.tsx
@@ -2,29 +2,35 @@ import Grid from "@mui/material/Grid";
 
 import Cell from "../atoms/Cell";
 import useGameLogic from "../../hooks/useGameLogic";
+import SquareSkeleton from "../molecules/SquareSkeleton";
 
 const Board: React.FC = () => {
-  const { isWon, board, handleBoardClick } = useGameLogic();
+  const { isWon, board, isLoading, handleBoardClick } = useGameLogic();
+
   return (
     <Grid container>
-      <Grid
-        item
-        display={"grid"}
-        gridTemplateColumns={"repeat(10, 1fr)"}
-        gridTemplateRows={"repeat(10, 1fr)"}
-        border={"5px solid #FFB300"}
-      >
-        {board.map((row, rowIndex) =>
-          row.map((cell, colIndex) => (
-            <Cell
-              key={`${rowIndex}-${colIndex}`}
-              isActive={!isWon}
-              value={cell}
-              onClick={() => handleBoardClick(rowIndex, colIndex)}
-            />
-          ))
-        )}
-      </Grid>
+      {isLoading ? (
+        <SquareSkeleton />
+      ) : (
+        <Grid
+          item
+          display={"grid"}
+          gridTemplateColumns={"repeat(10, 1fr)"}
+          gridTemplateRows={"repeat(10, 1fr)"}
+          border={"5px solid #FFB300"}
+        >
+          {board.map((row, rowIndex) =>
+            row.map((cell, colIndex) => (
+              <Cell
+                key={`${rowIndex}-${colIndex}`}
+                isActive={!isWon}
+                value={cell}
+                onClick={() => handleBoardClick(rowIndex, colIndex)}
+              />
+            ))
+          )}
+        </Grid>
+      )}
     </Grid>
   );
 };
