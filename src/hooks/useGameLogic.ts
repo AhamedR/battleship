987455afb925590ba6e47aa882aa8118814c8ca1
Ahamed <ahamedrasheedhot@gmail.com>
2024-07-22 08:46:51 +0530

    chore: minor code enhancements

--- src/hooks/useGameLogic.ts
+++ src/hooks/useGameLogic.ts
@@ -3,7 +3,7 @@ import { GameContext, GameContextType } from '../contexts/GameContext';
 
 const useGameLogic = (): GameContextType => {
   const context = useContext(GameContext);
-  console.log(context);
+
   if (!context) {
     throw new Error('useGameLogic must be used within a GameProvider');
   }
